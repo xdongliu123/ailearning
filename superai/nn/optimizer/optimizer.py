d233ff7a4c85af6127c2e114b2e6de3a0a7e73f9
zoe <zoe.liu@everbridge.com>
2019-11-19 11:11:02 +0800

    fix the issue of adam optimizer, couldn't update parameters in model layers

--- superai/nn/optimizer/optimizer.py
+++ superai/nn/optimizer/optimizer.py
@@ -4,14 +4,13 @@ import numpy as np
 
 
 class Adam:
-    def __init__(self, lr=0.1, beta1=0.9, beta2=0.999, epsilon=1e-8):
-        self.lr = lr
+    def __init__(self, beta1=0.9, beta2=0.999, epsilon=1e-8):
         self.beta1 = beta1
         self.beta2 = beta2
         self.epsilon = epsilon
 
     def run(self, model, X, y):
-        self.init_parameters(model)
+        self.init_adam_grads(model)
         config = model.config
         t = 0
         iteration_count = config["iteration_count"]
@@ -24,6 +23,7 @@ class Adam:
             for batch in batches:
                 t = t + 1
                 batch_X, batch_y = batch
+
                 Z = model.forward_propagation(batch_X)
                 A = softmax(Z)
                 loss, grad = softmaxloss(Z, batch_y)
@@ -31,44 +31,38 @@ class Adam:
                     print("iteration{0}  cost:{1}, accuracy:{2}".format(t, loss, accuracy(A, batch_y)))
                 # l2 regularization
                 self.back_propagation(model, grad, t)
-                self.update_parameters(model)
+                self.update_parameters(model, t)
 
-    def init_parameters(self, model):
-        self.optimizer_parameters = []
-        for hidden in model.layers:
-            parameters = []
-            for param in hidden.params:
-                v_p = np.zeros(param.shape)
-                v_p_corrected = np.zeros(param.shape)
-                s_p = np.zeros(param.shape)
-                s_p_corrected = np.zeros(param.shape)
-                parameters.append([v_p, v_p_corrected, s_p, s_p_corrected])
-            self.optimizer_parameters.append(parameters)
+    def init_adam_grads(self, model):
+        self.optimizer_grads = [(np.zeros(param.shape), np.zeros(param.shape), np.zeros(param.shape), np.zeros(param.shape)) for hidden in model.layers for param in hidden.params]
 
     def back_propagation(self, model, grad, t):
         model.back_propagation(grad)
         # update adam derivations
-        layer_count = len(self.optimizer_parameters)
-        for i in range(layer_count):
-            parameters = self.optimizer_parameters[i]
-            model_parameters = model.layers[i].grads
-            param_count = len(model_parameters)
-            for j in range(param_count):
-                opt_ps = parameters[j]
-                org_p = model_parameters[j]
-                v_p = self.beta1 * opt_ps[0] + (1. - self.beta1) * org_p
+        base = 0
+        for i in range(len(model.layers)):
+            for j in range(len(model.layers[i].grads)):
+                index = base + j
+                v_p, v_p_c, s_p, s_p_c = self.optimizer_grads[index]
+                grad = model.layers[i].grads[j]
+                v_p = self.beta1 * v_p + (1. - self.beta1) * grad
                 v_p_corrected = v_p / (1. - self.beta1**(t))
-                s_p = self.beta2 * opt_ps[2] + (1. - self.beta2) * org_p**(2)
+                s_p = self.beta2 * s_p + (1. - self.beta2) * grad**(2)
                 s_p_corrected = s_p / (1. - self.beta2**(t))
-                self.optimizer_parameters[i][j] = [v_p, v_p_corrected, s_p, s_p_corrected]
+                self.optimizer_grads[index] = (v_p, v_p_corrected, s_p, s_p_corrected)
+            base = base + len(model.layers[i].grads)
 
-    def update_parameters(self, model):
-        layer_count = len(self.optimizer_parameters)
-        for i in range(layer_count):
-            model_layer = model.layers[i]
-            param_count = len(model_layer.params)
-            for j in range(param_count):
-                opt_ps = self.optimizer_parameters[i][j]
-                v_p_corrected = opt_ps[1]
-                s_p_corrected = opt_ps[3]
-                model_layer.params[j] = model_layer.params[j] - self.lr * v_p_corrected / (np.sqrt(s_p_corrected) + self.epsilon)
+    def update_parameters(self, model, t):
+        # print("update_parameters: %x" % id(model))
+        lr = model.config["learning_rate"]
+        lr_t = lr * (np.sqrt(1. - np.power(self.beta2, t)) / (1. - np.power(self.beta1, t)))
+        base = 0
+        for i in range(len(model.layers)):
+            adam_grads = []
+            for j in range(len(model.layers[i].params)):
+                index = base + j
+                v_p, v_p_c, s_p, s_p_c = self.optimizer_grads[index]
+                adam_grads.append(v_p_c / (np.sqrt(s_p_c) + self.epsilon))
+            # don't update model layer's parameter of list type from outer, it is tricky
+            model.layers[i].update_parameters(adam_grads, lr_t)
+            base = base + len(model.layers[i].params)
